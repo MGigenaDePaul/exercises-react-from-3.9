09202472c034646bf3deeb574290db253d7a426a
Miqueas <miqueasgigena18@gmail.com>
2025-07-26 13:17:05 -0300

    Exercise 3.16 added new error handler middleware

--- backend/part3/phonebook/index.js
+++ backend/part3/phonebook/index.js
@@ -8,15 +8,25 @@ const app = express()
 
 let persons = []
 
+app.use(express.static('dist'))
+
 app.use(express.json())
 
 app.use(morgan(':method :url :response-time ms :body')) // custom token formats
 
-app.use(express.static('dist'))
-
 morgan.token('body', function getBody(req, res) {
     return JSON.stringify(req.body)
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({error: 'malformatted id'})
+    }
+
+    next(error)
+}
 
 app.get('/', (request, response) => {
     response.send('Hello persons')
@@ -68,13 +78,15 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id)
         .then(result => {
             response.status(204).end()
         })
-        .catch(error => console.log(error))
+        .catch(error => next(error))
 })
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
