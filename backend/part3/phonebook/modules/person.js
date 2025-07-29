fd815de3f72d53c293edf0edf587d49cd5863889
Miqueas <miqueasgigena18@gmail.com>
2025-07-29 17:45:21 -0300

    Exercise 3.20*

--- backend/part3/phonebook/modules/person.js
+++ backend/part3/phonebook/modules/person.js
@@ -20,7 +20,18 @@ const personSchema = new mongoose.Schema({
         minLength: 3,
         required: true
     },
-    number: String
+    number: {
+        type: String, 
+        validate: {
+            validator: function(v) {
+                v = v.trim()
+                return (/\d{2}-\d{6,}/.test(v) || /\d{3}-\d{5,}/.test(v))  // 2 or 3 numbers in the beggining
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        minLength: 8,
+        required: true 
+    }
 })
 
 personSchema.set('toJSON', {
